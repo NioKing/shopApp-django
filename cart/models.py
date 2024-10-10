f25bdc8ec103a72ed8642c50197254478c5a1c00
Kirill Ulichkin <nio@example.com>
2024-10-10 04:31:54 +0300

    added cart products relationship

--- cart/models.py
+++ cart/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from authentication.models import User
 from django.utils.translation import gettext_lazy as _
-
+from product.models import Product
 
 class Cart(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="cart")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="cart", editable=False)
     updated_at = models.DateTimeField(auto_now=True)
+    products = models.ManyToManyField(Product, blank=True, related_name='carts')
+
 
     class Meta:
         verbose_name = _("Cart")
