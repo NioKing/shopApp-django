cc25afc8bd084a0d446548324dc0c74c3f174d3c
Kirill Ulichkin <nio@example.com>
2024-10-13 02:49:40 +0300

    smtp fix

--- notification/signals.py
+++ notification/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from authentication.models import User
 from django.core.mail import send_mail
@@ -6,12 +6,21 @@ from django.core.mail import send_mail
 @receiver(post_save, sender=User)
 def send_email(sender, instance, created, **kwargs):
     if created:
-        print(f"New user created: {instance.email}")
-        # send_mail(
-        #     'Welcome to our shop!',
-        #     'Thank you for registering!',
-        #     'noreply@example.com',
-        #     [instance.email],
-        #     fail_silently=False,
-        # )
+        send_mail(
+            'Welcome to our shop!',
+            'Thank you for registering!',
+            'shop@app.com',
+            [instance.email],
+            fail_silently=False,
+        )
 
+
+@receiver(post_delete, sender=User)
+def send_email_on_delete(sender, instance, **kwargs):
+    send_mail(
+        'User deleted',
+        f'User {instance.email} was deleted',
+        'shop@app.com',
+        [instance.email],
+        fail_silently=False,
+    )
