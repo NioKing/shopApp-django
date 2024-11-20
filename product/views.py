0b35048037fd74ed3dff52f1ff1a95e9ee684eda
Kirill Ulichkin <ulichkin.kirill@yandex.ru>
2024-11-20 04:03:52 +0300

    product views fix and reqs changes

--- product/views.py
+++ product/views.py
@@ -24,7 +24,8 @@ def product_list_create(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
